4d235ce1fa8accdf787d00e2cf08b4f0a3e4b730
prakaski <kiran.prakash@sunquestinfo.com>
2021-11-25 17:19:04 +0530

    Commented denied scenario and uncommented subscribe scenario

--- Youtube.Sockets/Handlers/WebSocketMessageHandler.cs
+++ Youtube.Sockets/Handlers/WebSocketMessageHandler.cs
@@ -20,11 +20,11 @@ namespace Youtube.Sockets.Handlers
             await base.OnConnected(socket);
             var socketId = Connections.GetId(socket);
             //var response = new { systemLoginId = 293461, loginModeId = 293461, ldapLoginId = 293461, userName = "Epic,User" };
-            //string response = "{'hub.mode':'subscribe','hub.topic':'fdb2f928-5546-4f52-87a0-0648e9ded065','hub.events':'epic/com.epic.userlogin.read,epic/com.epic.userlogout.read,epic/com.epic.userhibernate.read','hub.lease-seconds':7200}";
-            //await SendMessageToAll($"{response}");
-            //Below code to test the denied scenario
-            string response = "{'hub.mode':'denied','hub.topic':'fdb2f928-5546-4f52-87a0-0648e9ded065','hub.events':'epic/com.epic.userlogin.read,epic/com.epic.userlogout.read,epic/com.epic.userhibernate.read','hub.reason':'Subscriber does not have valid authorization for one or more requested events.'}";
+            string response = "{'hub.mode':'subscribe','hub.topic':'fdb2f928-5546-4f52-87a0-0648e9ded065','hub.events':'epic/com.epic.userlogin.read,epic/com.epic.userlogout.read,epic/com.epic.userhibernate.read','hub.lease-seconds':7200}";
             await SendMessageToAll($"{response}");
+            //Below code to test the denied scenario
+            //string response = "{'hub.mode':'denied','hub.topic':'fdb2f928-5546-4f52-87a0-0648e9ded065','hub.events':'epic/com.epic.userlogin.read,epic/com.epic.userlogout.read,epic/com.epic.userhibernate.read','hub.reason':'Subscriber does not have valid authorization for one or more requested events.'}";
+            //await SendMessageToAll($"{response}");
             string message = "{'systemLoginId':293461,'loginModeId':293461,'ldapLoginId':293461,'userName':'Epic,User' }";
             await SendMessageToAll($"{message}");
             //await SendMessageToAll($"{socketId} just joined the party");
