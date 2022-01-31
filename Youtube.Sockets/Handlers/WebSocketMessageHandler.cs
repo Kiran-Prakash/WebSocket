763a32f84aa714e66645cc7e82c18c3b2dd10df5
prakaski <kiran.prakash@sunquestinfo.com>
2022-01-31 10:30:00 +0530

    Modified outgoing wss object

--- Youtube.Sockets/Handlers/WebSocketMessageHandler.cs
+++ Youtube.Sockets/Handlers/WebSocketMessageHandler.cs
@@ -25,7 +25,7 @@ namespace Youtube.Sockets.Handlers
             //Below code to test the denied scenario
             //string response = "{'hub.mode':'denied','hub.topic':'fdb2f928-5546-4f52-87a0-0648e9ded065','hub.events':'epic/com.epic.userlogin.read,epic/com.epic.userlogout.read,epic/com.epic.userhibernate.read','hub.reason':'Subscriber does not have valid authorization for one or more requested events.'}";
             //await SendMessageToAll($"{response}");
-            string message = "{'systemLoginId':293461,'loginModeId':293461,'ldapLoginId':293461,'userName':'Epic,User' }";
+            string message = "{ \"timestamp\": \"2022 - 01 - 24T14: 58:58.73\", \"id\": \"JVoD0v9rR0uoOmqw2iEUyQ\", \"event\": { \"hub.topic\": \"TORxeQrHb06e - eu5z0TV6Q\", \"hub.event\": \"com.epic.userlogin\", \"context\": [ { \"systemLoginId\": \"SUNQUESTONE\", \"loginModeId\": \"SUNQUESTONE\", \"ldapLoginId\": \"SUNQUESTONE\", \"userName\": \"SUNQUEST, USER\" } ] } }";
             await SendMessageToAll($"{message}");
             //await SendMessageToAll($"{socketId} just joined the party");
         }
